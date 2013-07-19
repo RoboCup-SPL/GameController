2056d4e0f8cb0e5546c89bd5e1fc8cd5a7b47dd8
yuzong <yuzong@tzi.de>
2013-07-19 14:26:03 +0200

    Some more analyzer docu

--- src/analyzer/LogInfo.java
+++ src/analyzer/LogInfo.java
@@ -14,24 +14,50 @@ import java.util.LinkedList;
 /**
  * @author: Michel Bartsch
  * 
+ * Instances of this class represent a log file. It can analyze some basic
+ * information of it´s log to guess, if it was made by a real game.
  */
 public class LogInfo
 {
+    /* A log should be written in this charset. */
     private final static String CHARSET = "UTF-8";
+    /* A team´s name which indicates, that this is not a real game´s log. */
     private final static String NOT_A_REAL_TEAM = "Invisibles";
+    /* Minimal duration in seconds between the first ready and the last
+     Finish a real game should have.*/
     private final static int MIN_DURATION = 18*60;
+    /* Number of log entries with general information at the beginning of
+     * every log, used to count the number of real actions. */
     private final static int NUM_OF_INFO_ENTRIES = 6;
     
+    /* The log file this instance belongs to. */
     public File file;
+    /* The version information found in the log. */
     public String version;
+    /* The league this log´s game was layed in. */
     public Rules league;
+    /* If the teams have kept there team colors during the game. */
     public boolean keepColors = false;
+    /* The teams`s names. */
     public String[] team = new String[2];
+    /* Time of the first ready. */
     public Date start;
+    /* Duration of the game in seconds (first ready to last finish) */
     public int duration;
+    /* All lines from the log file. */
     public LinkedList<String> lines = new LinkedList<String>();
+    /* If something odd happens while parsing this log, it will be written
+     * into this string. */
     public String parseErrors = "";
     
+    
+    /**
+     * Creates a new LogInfo.
+     * It will read the whole log into a list of strings and launch a parsing
+     * method to collect some basic information.
+     * 
+     * @param log   The log file to read and represent.
+     */
     public LogInfo(File log)
     {
         file = log;
@@ -56,48 +82,76 @@ public class LogInfo
         Parser.info(this);
     }
     
+    /**
+     * Makes a guess if this is a real game´s log based on all basic
+     * information.
+     * 
+     * @return  True, if this looks like a real game´s log or false, if not.
+     */
     public boolean isRealLog()
     {
         return isRealVersion()
                 && isRealLeague()
-                && isRealTeamOne()
-                && isRealTeamTwo()
+                && isRealTeam(true)
+                && isRealTeam(false)
                 && isRealDuration();
     }
     
+    /**
+     * Makes a guess if this is a real game´s log based on it´s version.
+     * 
+     * @return  True, if this looks like a real game´s log or false, if not.
+     */
     private boolean isRealVersion()
     {
         return version == null ? false : version.equals(controller.Main.version);
     }
     
+    /**
+     * Makes a guess if this is a real game´s log based on it´s league.
+     * 
+     * @return  True, if this looks like a real game´s log or false, if not.
+     */
     private boolean isRealLeague()
     {
         return league != null;
     }
     
-    private boolean isRealTeamOne()
+    /**
+     * Makes a guess if this is a real game´s log based on a team name.
+     * 
+     * @param firstTeam     True, if you want to check the first team´s name,
+     *                      or false for the second.
+     * @return      True, if this looks like a real game´s log or false, if not.
+     */
+    private boolean isRealTeam(boolean firstTeam)
     {
-        return team[0] == null ? false : !team[0].equals(NOT_A_REAL_TEAM);
+        return team[firstTeam ? 0 : 1] == null ? false : !team[firstTeam ? 0 : 1].equals(NOT_A_REAL_TEAM);
     }
     
-    private boolean isRealTeamTwo()
-    {
-        return team[1] == null ? false : !team[1].equals(NOT_A_REAL_TEAM);
-    }
-    
+    /**
+     * Makes a guess if this is a real game´s log based on it´s duration.
+     * 
+     * @return  True, if this looks like a real game´s log or false, if not.
+     */
     private boolean isRealDuration()
     {
         return duration > MIN_DURATION;
     }
     
+    /**
+     * This method writes all basic information into a string.
+     * 
+     * @return  Nice looking string with all basic information.
+     */
     public String getInfo()
     {
         return GUI.HTML
                 + (isRealVersion() ? version : GUI.HTML_RED + version + GUI.HTML_END) + GUI.HTML_LF
                 + (isRealVersion() ? league.leagueName : GUI.HTML_RED + league + GUI.HTML_END) + GUI.HTML_LF
-                + (isRealTeamOne() ? team[0] : GUI.HTML_RED + team[0] + GUI.HTML_END)
+                + (isRealTeam(true) ? team[0] : GUI.HTML_RED + team[0] + GUI.HTML_END)
                 + " vs "
-                + (isRealTeamTwo() ? team[1] : GUI.HTML_RED + team[1] + GUI.HTML_END) + GUI.HTML_LF
+                + (isRealTeam(false) ? team[1] : GUI.HTML_RED + team[1] + GUI.HTML_END) + GUI.HTML_LF
                 + (keepColors ? "No Color Change" : "Color Change") + GUI.HTML_LF
                 + (start != null ? start : GUI.HTML_RED + start + GUI.HTML_END) + " starting" + GUI.HTML_LF
                 + (isRealDuration() ? duration : GUI.HTML_RED + duration + GUI.HTML_END) + " seconds" + GUI.HTML_LF
