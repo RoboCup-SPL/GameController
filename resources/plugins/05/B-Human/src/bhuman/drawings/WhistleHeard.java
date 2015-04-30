3ad87c6eea7d2ef0ff80a6614cc181c99654d243
Felix Thielke <fthielke@fb3.uni-bremen.de>
2015-04-30 13:25:12 +0200

    display heard whistles in B-Human plugin

---
+++ resources/plugins/05/B-Human/src/bhuman/drawings/WhistleHeard.java
@@ -0,0 +1,54 @@
+package bhuman.drawings;
+
+import bhuman.message.BHumanMessage;
+import bhuman.message.messages.Whistle;
+import com.jogamp.opengl.GL2;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import teamcomm.gui.Camera;
+import teamcomm.data.RobotState;
+import teamcomm.gui.TextureLoader;
+import teamcomm.gui.drawings.Image;
+import teamcomm.gui.drawings.PerPlayer;
+
+/**
+ *
+ * @author Felix Thielke
+ */
+public class WhistleHeard extends PerPlayer {
+
+    @Override
+    public void draw(final GL2 gl, final RobotState rs, final Camera camera) {
+        if (rs.getLastMessage() != null && rs.getLastMessage() instanceof BHumanMessage) {
+            final BHumanMessage msg = (BHumanMessage) rs.getLastMessage();
+            final Whistle whistle = msg.queue.getMessage(Whistle.class);
+            if (whistle != null && whistle.confidenceOfLastWhistleDetection > 50 && whistle.lastTimeOfIncomingSound >= msg.queue.getTimestamp() - 200) {
+                gl.glPushMatrix();
+                gl.glTranslatef(msg.pose[0] / 1000.f, msg.pose[1] / 1000.f, 1);
+                camera.turnTowardsCamera(gl);
+                try {
+                    final File f = new File("plugins/05/resources/whistle.png").getAbsoluteFile();
+                    Image.drawImage(gl, TextureLoader.getInstance().loadTexture(gl, f), 0, 0, 0.2f);
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(null,
+                            "Error loading texture: " + ex.getMessage(),
+                            ex.getClass().getSimpleName(),
+                            JOptionPane.ERROR_MESSAGE);
+                }
+                gl.glPopMatrix();
+            }
+        }
+    }
+
+    @Override
+    public boolean hasAlpha() {
+        return true;
+    }
+
+    @Override
+    public int getPriority() {
+        return 9;
+    }
+
+}
---
+++ resources/plugins/05/B-Human/src/bhuman/message/messages/Whistle.java
@@ -0,0 +1,42 @@
+package bhuman.message.messages;
+
+import bhuman.message.Message;
+import bhuman.message.data.NativeReaders;
+import bhuman.message.data.SimpleStreamReader;
+import java.nio.ByteBuffer;
+
+/**
+ *
+ * @author Felix Thielke
+ */
+public class Whistle extends Message<Whistle> implements SimpleStreamReader<Whistle> {
+
+    /**
+     * Confidence based on hearing capability.
+     */
+    public byte confidenceOfLastWhistleDetection;
+    /**
+     * Timestamp.
+     */
+    public long lastTimeWhistleDetected;
+    /**
+     * The last point of time when the robot received audio data.
+     */
+    public long lastTimeOfIncomingSound;
+
+    @Override
+    public Whistle read(final ByteBuffer stream) {
+        confidenceOfLastWhistleDetection = NativeReaders.scharReader.read(stream);
+        lastTimeWhistleDetected = NativeReaders.uintReader.read(stream);
+        lastTimeOfIncomingSound = NativeReaders.uintReader.read(stream);
+
+        return this;
+    }
+
+    @Override
+    public int getStreamedSize() {
+        return NativeReaders.scharReader.getStreamedSize()
+                + NativeReaders.uintReader.getStreamedSize() * 2;
+    }
+
+}
