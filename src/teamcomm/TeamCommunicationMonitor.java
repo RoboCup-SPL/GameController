61fa5fb5fec0f41720c5e025476d62c446cda8da
Felix Thielke <fthielke@fb3.uni-bremen.de>
2015-10-01 12:00:34 +0200

    added silent mode to the TCM

--- src/teamcomm/TeamCommunicationMonitor.java
+++ src/teamcomm/TeamCommunicationMonitor.java
@@ -20,6 +20,8 @@ import teamcomm.net.logging.Logger;
  */
 public class TeamCommunicationMonitor {
 
+    private static boolean silentMode = false;
+
     private static boolean shutdown = false;
     private static final Object shutdownMutex = new Object();
 
@@ -32,26 +34,42 @@ public class TeamCommunicationMonitor {
         GameControlDataReceiver gcDataReceiver = null;
         SPLStandardMessageReceiver receiver = null;
 
+        parseArgs(args);
+
         // try to acquire the application lock
         final ApplicationLock applicationLock = new ApplicationLock("TeamCommunicationMonitor");
         try {
             if (!applicationLock.acquire()) {
-                JOptionPane.showMessageDialog(null,
-                        "An instance of TeamCommunicationMonitor already exists.",
-                        "Multiple instances",
-                        JOptionPane.WARNING_MESSAGE);
+                if (silentMode) {
+                    System.out.println("An instance of TeamCommunicationMonitor already exists.");
+                } else {
+                    JOptionPane.showMessageDialog(null,
+                            "An instance of TeamCommunicationMonitor already exists.",
+                            "Multiple instances",
+                            JOptionPane.WARNING_MESSAGE);
+                }
                 System.exit(0);
             }
         } catch (IOException | HeadlessException e) {
-            JOptionPane.showMessageDialog(null,
-                    "Error while trying to acquire the application lock.",
-                    e.getClass().getSimpleName(),
-                    JOptionPane.ERROR_MESSAGE);
+            if (silentMode) {
+                System.out.println("Error while trying to acquire the application lock.");
+            } else {
+                JOptionPane.showMessageDialog(null,
+                        "Error while trying to acquire the application lock.",
+                        e.getClass().getSimpleName(),
+                        JOptionPane.ERROR_MESSAGE);
+            }
             System.exit(-1);
         }
 
-        // Initialize the JOGL profile for 3D drawing
-        GLProfile.initSingleton();
+        if (silentMode) {
+            System.out.println("Team Communication Monitor was started in silent mode.\nMessages will be received and logged but not displayed.");
+        }
+
+        if (!silentMode) {
+            // Initialize the JOGL profile for 3D drawing
+            GLProfile.initSingleton();
+        }
 
         // Initialize listener for GameController messages
         try {
@@ -68,7 +86,7 @@ public class TeamCommunicationMonitor {
         receiver = SPLStandardMessageReceiver.getInstance();
 
         // Initialize robot view part of the GUI
-        final MainWindow robotView = new MainWindow();
+        final MainWindow robotView = silentMode ? null : new MainWindow();
 
         // Start threads
         gcDataReceiver.start();
@@ -94,7 +112,9 @@ public class TeamCommunicationMonitor {
         GameState.getInstance().shutdown();
         receiver.interrupt();
         gcDataReceiver.interrupt();
-        robotView.terminate();
+        if (robotView != null) {
+            robotView.terminate();
+        }
         LogReplayer.getInstance().close();
         Logger.getInstance().closeLogfile();
 
@@ -107,6 +127,27 @@ public class TeamCommunicationMonitor {
 
         // Force exit
         System.exit(0);
+    }
+
+    private static final String ARG_HELP_SHORT = "-h";
+    private static final String ARG_HELP = "--help";
+    private static final String ARG_SILENT_SHORT = "-s";
+    private static final String ARG_SILENT = "--silent";
+
+    private static void parseArgs(final String[] args) {
+        for (final String arg : args) {
+            switch (arg) {
+                case ARG_HELP_SHORT:
+                case ARG_HELP:
+                    System.out.println("Usage: java -jar TeamCommunicationMonitor.jar {options}"
+                            + "\n  (-h | --help)                   display help"
+                            + "\n  (-s | --silent)                 start in silent mode");
+                    System.exit(0);
+                case ARG_SILENT_SHORT:
+                case ARG_SILENT:
+                    silentMode = true;
+            }
+        }
     }
 
     /**
