5b2f0bee1bf81d7c8de4a64f8feeecd8bb4e99c3
Dennis Schürholz <bhuman@dennisschuerholz.de>
2016-03-15 18:18:39 +0100

    Rules: normalize comment line endings

--- src/data/SPLDropIn.java
+++ src/data/SPLDropIn.java
@@ -16,9 +16,9 @@ public class SPLDropIn extends SPL
         leagueDirectory = "spl_dropin";
         /** How many robots are in a team. */
         teamSize = robotsPlaying;
-        /** Defines if coach is available **/
+        /** Defines if coach is available */
         isCoachAvailable = false;
-        /** If true, the drop-in player competition is active*/
+        /** If true, the drop-in player competition is active */
         dropInPlayerMode = true;
         /** On how many pushings is a robot ejected. */
         pushesToEjection = new int[] {};
