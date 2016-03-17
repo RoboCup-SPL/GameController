910c35c31d18831cafb024c3cf23b30af3de814a
Dennis Schürholz <bhuman@dennisschuerholz.de>
2016-03-18 00:59:57 +0100

    Introducing the -i parameter replacing -b
    - new -i/--interface parameter
      It is now possible to set a network interface to listen on (relevant in SPL as
      broadcast messages get dropped in that way) using this new parameter.
    - removed -b/--broadcast parameter
      Due to the selection of an interface it is no longer necessary to supply a
      broadcast address with --broadcast as it can be retreived from the interface.
    - if no interface is specified an automatic choice will be made:
      The first (in an ordering given by the operating system, may or may not be
      alphabetical) up and running interface with an assigned IPv4 address will be
      selected.

--- src/controller/net/GameControlReturnDataReceiver.java
+++ src/controller/net/GameControlReturnDataReceiver.java
@@ -5,12 +5,10 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
-import java.util.Enumeration;
 import java.util.regex.Pattern;
 
 import common.Log;
@@ -35,50 +33,30 @@ public class GameControlReturnDataReceiver extends Thread
     /** The instance of the singleton. */
     private static GameControlReturnDataReceiver instance;
 
-    private static Pattern LOOPBACK_PATTERN = Pattern.compile("^127(\\.(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)){3}$");
-
     /** The used socket to receive the packages. */
     private final DatagramSocket datagramSocket;
 
     /**
      * Creates a new Receiver.
      *
-     * @param pattern the {@link Pattern} the local address has to match against.<br />
+     * @param address the InetAddress to listen on.<br />
      *        Only applied if {@link Rules#dropBroadcastMessages rule.dropBroadcastMessages} is set
      *        to true.
      * @throws SocketException the an error occurs while creating the socket
      * @throws UnknownHostException if (internally chosen) inet-address is not valid or no network
      *         device is bound to an address matching the regex (ignoring loopback interfaces)
      */
-    private GameControlReturnDataReceiver(final Pattern pattern) throws SocketException, UnknownHostException
+    private GameControlReturnDataReceiver(final InetAddress address) throws SocketException, UnknownHostException
     {
         datagramSocket = new DatagramSocket(null);
         datagramSocket.setReuseAddress(true);
         datagramSocket.setSoTimeout(500);
         if (Rules.league.dropBroadcastMessages) {
-            Enumeration<NetworkInterface> nifs = NetworkInterface.getNetworkInterfaces();
-            nifs: while (nifs.hasMoreElements()) {
-                Enumeration<InetAddress> ips = nifs.nextElement().getInetAddresses();
-                while (ips.hasMoreElements()) {
-                    String addr = ips.nextElement().getHostAddress();
-                    if (pattern.matcher(addr).matches()
-                            && !LOOPBACK_PATTERN.matcher(addr).matches()) {
-                        try {
-                            datagramSocket.bind(new InetSocketAddress(InetAddress.getByName(addr),
-                                    GameControlData.GAMECONTROLLER_RETURNDATA_PORT));
-                        } catch (UnknownHostException e) {
-                            // this can never happen, the address got retrieved from the system so it's known
-                        }
-                        System.out.printf("Listening on %s:%d%n", addr, GameControlData.GAMECONTROLLER_RETURNDATA_PORT);
-                        break nifs;
-                    }
-                }
-            }
-            if(!datagramSocket.isBound()) {
-                throw new UnknownHostException("no local address matches the regex (excluding loopback interfaces)");
-            }
+            datagramSocket.bind(new InetSocketAddress(address,
+                    GameControlData.GAMECONTROLLER_RETURNDATA_PORT));
         } else {
-            datagramSocket.bind(new InetSocketAddress(GameControlData.GAMECONTROLLER_RETURNDATA_PORT));
+            datagramSocket
+                    .bind(new InetSocketAddress(GameControlData.GAMECONTROLLER_RETURNDATA_PORT));
         }
     }
 
@@ -86,7 +64,7 @@ public class GameControlReturnDataReceiver extends Thread
      * Initializes the GameControlReturnDataReceiver. This needs to be called before
      * {@link #getInstance()} is available.
      * 
-     * @param addresspattern the {@link Pattern} the local address has to match against.<br />
+     * @param address the InetAddress to listen on.<br />
      *        Only applied if {@link Rules#dropBroadcastMessages rule.dropBroadcastMessages} is set
      *        to true.
      * @throws SocketException if an error occurs while creating the socket
@@ -94,14 +72,12 @@ public class GameControlReturnDataReceiver extends Thread
      *         device is bound to an address matching the regex (ignoring loopback interfaces)
      * @throws IllegalStateException if the Receiver is already initialized
      */
-    public synchronized static void initialize(final Pattern addresspattern) throws SocketException, UnknownHostException
+    public synchronized static void initialize(final InetAddress address) throws SocketException, UnknownHostException
     {
         if (instance != null) {
             throw new IllegalStateException("receiver is already initialized");
-        } if (addresspattern == null) {
-            throw new IllegalArgumentException("null is not avalid pattern");
         } else {
-            instance = new GameControlReturnDataReceiver(addresspattern);
+            instance = new GameControlReturnDataReceiver(address);
         }
     }
 
