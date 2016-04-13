0d8bece841f226420726c7eda953ec0647e6e7c7
Felix Thielke <fthielke@fb3.uni-bremen.de>
2016-04-13 13:21:41 +0200

    Added a drawing for a small SPL field (as used in 2012)

---
+++ src/teamcomm/gui/drawings/common/FieldSmall.java
@@ -0,0 +1,36 @@
+package teamcomm.gui.drawings.common;
+
+import com.jogamp.opengl.GL2;
+import teamcomm.gui.Camera;
+import teamcomm.gui.drawings.RoSi2Loader;
+import teamcomm.gui.drawings.Static;
+
+/**
+ * Drawing for the field.
+ *
+ * @author Felix Thielke
+ */
+public class FieldSmall extends Static {
+
+    @Override
+    protected void init(GL2 gl) {
+        RoSi2Loader.getInstance().cacheModels(gl, new String[]{"fieldSmall"});
+        setActive(false);
+    }
+
+    @Override
+    public void draw(final GL2 gl, final Camera camera) {
+        gl.glCallList(RoSi2Loader.getInstance().loadModel(gl, "fieldSmall"));
+    }
+
+    @Override
+    public boolean hasAlpha() {
+        return true;
+    }
+
+    @Override
+    public int getPriority() {
+        return 1000;
+    }
+
+}
