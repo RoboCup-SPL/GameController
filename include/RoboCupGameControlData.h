92ffd2ff8d056b8b181b77784f3b251d76dc1322
Mario Grobler <grobler@uni-bremen.de>
2018-02-23 14:48:52 +0100

    whoopsiwhoops

--- include/RoboCupGameControlData.h
+++ include/RoboCupGameControlData.h
@@ -31,6 +31,7 @@
 
 #define GAMETYPE_NORMALTEAM         0
 #define GAMETYPE_MIXEDTEAM          1
+#define GAMETYPE_GENERAL_PENALTY_KICK 2;
 
 #define STATE_INITIAL               0
 #define STATE_READY                 1
@@ -98,7 +99,7 @@ struct RoboCupGameControlData
   uint8_t packetNumber;         // number incremented with each packet sent (with wraparound)
   uint8_t playersPerTeam;       // the number of players on a team
   uint8_t competitionPhase : 4; // phase of the game (GAMEPHASE_ROUNDROBIN, GAMEPHASE_PLAYOFF) 
-  uint8_t competitionType : 4;  // type of the game (GAMETYPE_NORMALTEAM, GAMETYPE_MIXEDTEAM) 
+  uint8_t competitionType : 4;  // type of the game (GAMETYPE_NORMALTEAM, GAMETYPE_MIXEDTEAM, GAMETYPE_GENERAL_PENALTY_KICK) 
   uint8_t state;                // state of the game (STATE_READY, STATE_PLAYING, etc)
   uint8_t firstHalf;            // 1 = game in first half, 0 otherwise
   uint8_t kickingTeam;          // the team number of the next team to kick off, free kick, DROPBALL etc.
