68df49b0c5ddf4919079ca58e4e664fac5fecf8b
Georg Felbinger <gfelbing@github.com>
2018-05-09 16:53:53 +0200

    display ttrb in seconds with 1 digit

--- resources/plugins/24/HULKs/src/hulks/drawings/TimeToReachBall.java
+++ resources/plugins/24/HULKs/src/hulks/drawings/TimeToReachBall.java
@@ -2,20 +2,13 @@ package hulks.drawings;
 
 import com.jogamp.opengl.GL2;
 import common.Log;
-import data.Rules;
 import hulks.message.HulksMessage;
-import hulks.message.HulksMessageParts;
-import teamcomm.data.GameState;
 import teamcomm.data.RobotState;
 import teamcomm.gui.Camera;
-import teamcomm.gui.drawings.Image;
-import teamcomm.gui.drawings.Text;
 import teamcomm.gui.drawings.PerPlayer;
-import teamcomm.gui.drawings.TextureLoader;
+import teamcomm.gui.drawings.Text;
 
-import javax.swing.*;
-import java.io.File;
-import java.io.IOException;
+import java.text.DecimalFormat;
 
 /**
  * Custom drawing for visualizing the current performing role.
@@ -23,6 +16,8 @@ import java.io.IOException;
  * @author Georg Felbinger
  */
 public class TimeToReachBall extends PerPlayer {
+
+    private static final DecimalFormat TTRB_FORMAT = new DecimalFormat("#.#");
 
     @Override
     public void draw(final GL2 gl, final RobotState rs, final Camera camera) {
@@ -34,8 +29,8 @@ public class TimeToReachBall extends PerPlayer {
                 gl.glPushMatrix();
                 gl.glTranslatef(msg.pose[0] / 1000.f, msg.pose[1] / 1000.f - 0.3f, 0.5f);
                 camera.turnTowardsCamera(gl);
-                    final long ttrb = msg.message.bhulks.timeWhenReachBall.getTimeSince(msg.message.bhulks.timestamp);
-                    Text.drawText(""+ttrb, 0, 0, 0.15f, new float[]{0,0,0});
+                final float ttrb = msg.message.bhulks.timeWhenReachBall.getTimeSince(msg.message.bhulks.timestamp) / 1000.f;
+                Text.drawText(TTRB_FORMAT.format(ttrb), 0, 0, 0.15f, new float[]{0, 0, 0});
                 gl.glPopMatrix();
             }
         }
