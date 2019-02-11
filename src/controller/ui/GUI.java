6d515b296b2220cace6a3cfff7c6cc3d77619b97
Arne Hasselbring <arha@uni-bremen.de>
2019-02-11 11:19:03 +0100

    Add team color to free kick buttons

--- src/controller/ui/GUI.java
+++ src/controller/ui/GUI.java
@@ -360,9 +360,9 @@ public class GUI extends JFrame implements GCGUI
         cornerKick = new JButton[2];
         goalFreeKick = new JButton[2];
         for (int i=0; i<2; i++) {                
-            goalFreeKick[i] = new Button(GOAL_FREE_KICK);
-            kickIn[i] = new Button(KICK_IN);
-            cornerKick[i] = new Button(CORNER_KICK);
+            goalFreeKick[i] = new Button(GOAL_FREE_KICK+" for "+Rules.league.teamColorName[data.team[i].teamColor]);
+            kickIn[i] = new Button(KICK_IN+" for "+Rules.league.teamColorName[data.team[i].teamColor]);
+            cornerKick[i] = new Button(CORNER_KICK+" for "+Rules.league.teamColorName[data.team[i].teamColor]);
         }
         
         //--mid--
@@ -1107,6 +1107,7 @@ public class GUI extends JFrame implements GCGUI
     {
         for (int i=0; i<2; i++) {
             kickIn[i].setEnabled(ActionBoard.kickIn[i].isLegal(data));
+            kickIn[i].setText(KICK_IN+" for "+Rules.league.teamColorName[data.team[i].teamColor]);
         }
     }
     
@@ -1119,6 +1120,7 @@ public class GUI extends JFrame implements GCGUI
     {
         for (int i=0; i<2; i++) {
             goalFreeKick[i].setEnabled(ActionBoard.goalFreeKick[i].isLegal(data));
+            goalFreeKick[i].setText(GOAL_FREE_KICK+" for "+Rules.league.teamColorName[data.team[i].teamColor]);
         }
     }
     
@@ -1131,6 +1133,7 @@ public class GUI extends JFrame implements GCGUI
     {
         for (int i=0; i<2; i++) {
             cornerKick[i].setEnabled(ActionBoard.cornerKick[i].isLegal(data));
+            cornerKick[i].setText(CORNER_KICK+" for "+Rules.league.teamColorName[data.team[i].teamColor]);
         }
     }
 
